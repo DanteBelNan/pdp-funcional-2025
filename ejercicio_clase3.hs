2249a266cd8b6c87341c6c345d89baef20fa4672
DanteBelNan <dantebelnan@gmail.com>
2025-12-19 00:26:41 +0000

    ejercicio 3 hecho

--- ejercicio_clase3.hs
+++ ejercicio_clase3.hs
@@ -23,20 +23,47 @@ politicos = [
 
 -- 2) Función promediosAlumnos
 -- Definir la función promediosAlumnos/1, que dada una lista de alumnos devuelve una lista de tuplas que tenga el alumno y el promedio (Consideramos la división entera para el promedio y usamos la función div).
--- Definiciones de datos para ejercicios 2, 4 y 5:
+-- Definiciones de datos para ejercicios 2,3 , 4 y 5 dada por el docente:
 type Nombre = String
 type Notas = [Int]
 data Persona = Alumno { nombre :: Nombre, notas :: Notas } deriving Show
+
+promedio :: Notas -> Int
+promedio [] = 0
+promedio notas = div (sum notas) (length notas)
+
+promedioAlumnos :: [Persona] -> [(String,Int)]
+promedioAlumnos alumnos = map (\(Alumno nombre notas) -> (nombre,promedio notas)) alumnos
+
+
+
+-- 3) Función promediosSinAplazos/1, que dada una lista de listas, devuelvan la lista 
+-- de los promedios de cada lista-elemento, excluyendo los que sean menores a 6 que no se cuentan
+
+promedioSinAplazos :: [Persona] -> [(String,Int)]
+promedioSinAplazos alumnos = map (\(Alumno nombre notas) -> (nombre, promedio(filter(>=6) notas))) alumnos
 
 -- 4) Función aprobó
 -- Definir la función aprobó/1, que dado un alumno devuelve True si el alumno aprobó.
 -- Aclaración: Se dice que un alumno aprobó si todas sus notas son 6 o más.
 
+aprobo :: Persona -> Bool
+-- aprobo persona = (\(Alumno nombre notas) -> all notaAprobada notas) persona
+-- notaAprobada :: Int -> Bool
+-- notaAprobada nota = (>=6) nota
+aprobo persona = all (>=6) (notas persona) -- un poco mejor
+
 -- 5) Función aprobaron
 -- Definir la función aprobaron/1, que dada una lista de alumnos, devuelve los nombres de los alumnos que aprobaron.
+aprobaron :: [Persona] -> [String]
+aprobaron alumnos = map nombre $ filter aprobo alumnos
 
 -- 6) Función productos
 -- Definir la función productos que dado una lista de nombres de productos y una lista de precios, devuelve una lista de tuplas.
 -- Consigna:
 -- Definirla usando zip.
+tuplasZip :: [String] -> [Int] -> [(String,Int)]
+tuplasZip productos precios = zip productos precios
 -- Definirla usando zipWith.
+tuplasZipWith :: [String] -> [Int] -> [(String,Int)]
+tuplasZipWith productos precios = zipWith (\producto precio -> (producto,precio)) productos precios
